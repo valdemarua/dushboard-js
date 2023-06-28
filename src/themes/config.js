7cac4b50ad914295db1cf8d893924d5fbcb8ba5f
valdemarua <valdemarua@users.noreply.github.com>
2023-06-28 20:15:06 +0300

    Export themeConfig from index.js

---
+++ src/themes/config.js
@@ -0,0 +1,15 @@
+export default themeConfig = {
+  palette: {
+    primary: "blue",
+    secondary: "green",
+  },
+  typography: {
+    fontSize: 15,
+    h3: {
+      fontSize: "1.20rem",
+      fontWeight: 500,
+      marginTop: "1rem",
+      marginBottom: "1rem",
+    },
+  }
+}
