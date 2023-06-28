34406ce762db1e06a09955b8648f3f84aab4b228
valdemarua <valdemarua@users.noreply.github.com>
2023-06-28 20:26:56 +0300

    Update theme config colors, disable ripple effect

--- src/themes/config.js
+++ src/themes/config.js
@@ -1,15 +1,26 @@
 export const themeConfig = {
   palette: {
-    primary: "blue",
-    secondary: "green",
-  },
-  typography: {
-    fontSize: 15,
-    h3: {
-      fontSize: "1.20rem",
-      fontWeight: 500,
-      marginTop: "1rem",
-      marginBottom: "1rem",
+    primary: {
+      main: "blue",
     },
+    secondary: {
+      main: "green",
+    },
+  },
+  // typography: {
+  //   fontSize: 15,
+  //   h3: {
+  //     fontSize: "1.20rem",
+  //     fontWeight: 500,
+  //     marginTop: "1rem",
+  //     marginBottom: "1rem",
+  //   },
+  // },
+  components: {
+    MuiButtonBase: {
+      defaultProps: {
+        disableRipple: true,
+      }
+    }
   }
 }
