255d0ee2e32e459067a002511b32841a4e0e26da
valdemarua <valdemarua@users.noreply.github.com>
2023-07-01 16:38:20 +0300

    Remove cmj module

--- dist/cjs/types/components/Page/Page.d.ts
+++
@@ -1,7 +0,0 @@
-import { ReactNode } from "react";
-interface PageProps {
-    title?: string;
-    children?: ReactNode;
-}
-declare const Page: ({ title, children }: PageProps) => import("react/jsx-runtime").JSX.Element;
-export default Page;
