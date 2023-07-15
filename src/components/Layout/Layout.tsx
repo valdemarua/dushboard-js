78c50d745ce876a864bb6281583a077c95a1cac4
valdemarua <valdemarua@users.noreply.github.com>
2023-07-15 14:39:14 +0300

    Remove width from appbar

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,17 +1,7 @@
-import {
-  AppBar,
-  CssBaseline,
-  Drawer,
-  List,
-  ListItem,
-  ListItemText,
-  Toolbar,
-} from "@mui/material";
+import { AppBar, Toolbar } from "@mui/material";
 import { Box } from "@mui/system";
 import React from "react";
 import { Sidebar } from "./Sidebar";
-
-const drawerWidth = 220;
 
 type LayoutProps = {
   children?: React.ReactNode;
@@ -22,11 +12,7 @@ export function Layout({ children }: LayoutProps) {
     <Box sx={{ display: "flex" }}>
       <Sidebar />
       <Box>
-        <AppBar
-          position="fixed"
-          elevation={0}
-          sx={{ width: `calc(100% - ${drawerWidth}px)`, ml: `{drawerWidth}px` }}
-        >
+        <AppBar position="fixed" elevation={0}>
           <Toolbar disableGutters sx={{ pl: 2 }}>
             <Box>asdf@asedf.com</Box>
           </Toolbar>
