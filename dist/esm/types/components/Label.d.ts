28107fa6e5171376bef094a4d2883cf5011ff2cc
valdemarua <valdemarua@users.noreply.github.com>
2023-10-15 16:23:39 +0300

    Add label component

---
+++ dist/esm/types/components/Label.d.ts
@@ -0,0 +1,6 @@
+type LabelProps = {
+    label: string;
+    color?: "primary" | "secondary" | "success" | "info" | "warning" | "error" | "grey";
+};
+export declare const Label: ({ label, color }: LabelProps) => import("react/jsx-runtime").JSX.Element;
+export {};
---
+++ src/components/Label.tsx
@@ -0,0 +1,44 @@
+import { Box, lighten } from "@mui/material";
+
+type LabelProps = {
+  label: string;
+  color?:
+    | "primary"
+    | "secondary"
+    | "success"
+    | "info"
+    | "warning"
+    | "error"
+    | "grey";
+};
+
+export const Label = ({ label, color = "grey" }: LabelProps) => {
+  const textColor = () => {
+    return color === "grey"
+      ? (theme: any) => theme.palette.grey
+      : (theme: any) => theme.palette[color].main;
+  };
+
+  const bgColor = () => {
+    return color === "grey"
+      ? (theme: any) => theme.palette.grey[100]
+      : (theme: any) => lighten(theme.palette[color].main, 0.9);
+  };
+
+  return (
+    <Box
+      component="span"
+      sx={{
+        textAlign: "center",
+        px: 1,
+        py: 0.5,
+        borderRadius: 1,
+        textTransform: "capitalize",
+        color: textColor(),
+        bgcolor: bgColor(),
+      }}
+    >
+      {label}
+    </Box>
+  );
+};
